98a3e4acfaca59bd2db65947b93caabf63d379ac
Naufal Rizq Ramadhan <naufalrizq100@gmail.com>
2023-02-04 15:29:28 +0700

    bagusin dikit

--- quiz_game.py
+++ quiz_game.py
@@ -6,49 +6,49 @@ if playing.lower() != "yes":
     quit()
 
 print("Oke! Mari bermain :3")
-score = 0
+nilai = 0
 
-print("====================")
-answer = input("Siapa menteri pendidikan sekarang? ")
-if answer.lower() == "nadiem makarim":
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+jawaban = input("Siapa menteri pendidikan sekarang? ")
+if jawaban.lower() == "nadiem makarim":
     print('Jawaban kamu benar!')
-    score += 1
-else:
-    print("Jawaban kamu salah!")
-
-print("====================")
-answer = input("Binatang yang bisa hidup di darat dan di air disebut? ")
-if answer.lower() == "amfibi":
-    print('Jawaban kamu benar!')
-    score += 1
-else:
-    print("Jawaban kamu salah!")
-
-print("====================")
-answer = input("Negara terluas keempat di Dunia adalah? ")
-if answer.lower() == "china":
-    print('Jawaban kamu benar!')
-    score += 1
-else:
-    print("Jawaban kamu salah!")
-
-print("====================")
-answer = input("Gunung tertinggi di Dunia adalah? ")
-if answer.lower() == "gunung everest":
-    print('Jawaban kamu benar!')
-    score += 1
+    nilai += 1
 else:
     print("Jawaban kamu salah!")
     
-print("====================")    
-answer = input("Arah jam 9 itu sama dengan arah mata angin ke? ")
-if answer.lower() == "barat":
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+jawaban = input("Binatang yang bisa hidup di darat dan di air disebut? ")
+if jawaban.lower() == "amfibi":
     print('Jawaban kamu benar!')
-    score += 1
+    nilai += 1
 else:
     print("Jawaban kamu salah!")
 
-print("===================")
-print("Kamu berhasil menjawab " + str(score) + " pertanyaan dengan benar!")
-print("Kamu mendapatkan nilai " + str((score / 5) * 100) + "%.")
-print("===================")
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+jawaban = input("Negara terluas keempat di Dunia adalah? ")
+if jawaban.lower() == "china":
+    print('Jawaban kamu benar!')
+    nilai += 1
+else:
+    print("Jawaban kamu salah!")
+    
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+jawaban = input("Gunung tertinggi di Dunia adalah? ")
+if jawaban.lower() == "gunung everest":
+    print('Jawaban kamu benar!')
+    nilai += 1
+else:
+    print("Jawaban kamu salah!")
+    
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")    
+jawaban = input("Arah jam 9 itu sama dengan arah mata angin ke? ")
+if jawaban.lower() == "barat":
+    print('Jawaban kamu benar!')
+    nilai += 1
+else:
+    print("Jawaban kamu salah!")
+
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+print("Kamu berhasil menjawab " + str(nilai) + " pertanyaan dengan benar!")
+print("Kamu mendapatkan nilai " + str((nilai / 5) * 100) + "%.")
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
--- tebak_angka.py
+++ tebak_angka.py
@@ -1,35 +1,37 @@
 import random
 
-top_of_range = input("Type a number: ")
+batas_angka = input("Masukkan batas angka yang akan ditebak: ")
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
+if batas_angka.isdigit():
+    batas_angka = int(batas_angka)
 
-    if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
+    if batas_angka <= 0:
+        print('Tidak boleh memasukkan angka kurang dari 0!')
         quit()
 else:
-    print('Please type a number next time.')
+    print('Tidak boleh memasukkan angka 0!')
     quit()
 
-random_number = random.randint(0, top_of_range)
-guesses = 0
+angka_random = random.randint(0, batas_angka)
+tebakan = 0
 
 while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
+    tebakan += 1
+    user_guess = input("Coba tebak angkanya: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
     else:
-        print('Please type a number next time.')
+        print('Coba lagi!.')
         continue
 
-    if user_guess == random_number:
-        print("You got it!")
+    if user_guess == angka_random:
+        print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+        print("Selamat, kamu berhasil menebak angkanya!")
         break
-    elif user_guess > random_number:
-        print("You were above the number!")
+    elif user_guess > angka_random:
+        print("Angka tebakanmu terlalu tinggi!")
     else:
-        print("You were below the number!")
+        print("Angka tebakanmu terlalu rendah!")
 
-print("You got it in", guesses, "guesses")
+print("Kamu berhasil dalam", tebakan, " kali tebakan")
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
